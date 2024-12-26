4500a39b2f9a3bf87f33ab82143797732b14fd98
Muhammad Zain Zameer <officialmuhammadzain45@gmail.com>
2024-12-27 00:25:28 +0500

    initial commit

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 # # -------- getting letters ---------
 # print("GETTING LETTERS FROM DOCUMENT :>: ")
-# f_getFirstLine = open("transTable.txt")
-# f_line_getFirstLine = f_getFirstLine.readline()
-# f_line_getFirstLine = f_line_getFirstLine.strip()
-# f_line_getFirstLine = f_line_getFirstLine.replace(' ','')
-# for i in range(0,len(f_line_getFirstLine)):
-#     print(f_line_getFirstLine[i])
-# print("\n")
+f_getFirstLine = open("transTable.txt")
+f_line_getFirstLine = f_getFirstLine.readline()
+f_line_getFirstLine = f_line_getFirstLine.strip()
+f_line_getFirstLine = f_line_getFirstLine.replace(' ','')
+print(f_line_getFirstLine)
+
+print("\n")
 # # -------- getting states -----------
 # print("GETTING STATES FROM DOCUMENT :>: ")
 starting_state = None
@@ -17,10 +17,12 @@ lines = f.readlines()[1:]
 lines = [line for line in lines if line.strip()]
 
 for i in range(0,len(lines)):
-    if(lines[i][3]=='-'):
-        starting_state = lines[i][0:2]
-    elif(lines[i][3]=='+'):
-        Ending_states.append(lines[i][0:2])
+    state_info = lines[i].split()
+    
+    if('-' in state_info):
+        starting_state = state_info[0]
+    elif('+' in state_info):
+        Ending_states.append(state_info[0])
 
 print("Starting state: "+starting_state)
 print("Ending states: "+str(Ending_states))
@@ -52,16 +54,23 @@ print("current step:\n")
 for i in range(0,len(fetched_strings)):
     current_state = starting_state
     current_rule = {}
+    reject = False
     for j in range(0,len(fetched_strings[i])):
         current_letter = fetched_strings[i][j]
-        for key,value in rules.items():
-            if(key==current_state):
-                current_rule = value
-        for item,value in current_rule.items():
-            if(current_letter==item):
-                current_state = value
-    if(current_state in Ending_states):
+        if(current_letter in f_line_getFirstLine):
+            for key,value in rules.items():
+                if(key==current_state):
+                    current_rule = value
+            for item,value in current_rule.items():
+                if(current_letter==item):
+                    current_state = value     
+        else:
+            reject=True      
+        
+    if((current_state in Ending_states) and reject==False):
         print(f"{fetched_strings[i]} (Accepted)")
+    elif(reject==True):
+        print(f"{fetched_strings[i]} (Rejected)")
     else:
         print(f"{fetched_strings[i]} (Rejected)")
 
